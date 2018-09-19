9e74d507b289dd3cbaa4e7ccdf82dab83c361a00
rtang23 <richard23496@gmail.com>
2018-09-19 11:10:55 +1000

    updated testing bit

--- gputraining.py
+++ gputraining.py
@@ -559,7 +559,37 @@ with tf.Session() as sess:
     print("Model loaded")
 
     for episode in range(1):
-        test_model(episode,test=False)
+        total_rewards = 0
+
+        state = env.reset()
+        state, stacked_frames = stack_frames(stacked_frames, state, True)
+
+        print("****************************************************")
+        print("EPISODE ", episode)
+
+        while True:
+            # Reshape the state
+            state = state.reshape((1, *state_size))
+            # Get action from Q-network
+            # Estimate the Qs values state
+            Qs = sess.run(DQNetwork.output, feed_dict={DQNetwork.inputs_: state})
+
+            # Take the biggest Q value (= the best action)
+            choice = np.argmax(Qs)
+
+            # Perform the action and get the next_state, reward, and done information
+            next_state, reward, done, _ = env.step(choice)
+            env.render()
+
+            total_rewards += reward
+
+            if done:
+                print ("Score", total_rewards)
+                total_test_rewards.append(total_rewards)
+                break
+
+            next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+            state = next_state
 
     env.close()
 
